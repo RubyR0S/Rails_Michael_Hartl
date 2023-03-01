a97d9eef72538c640e0019d1dfd66a85487f7750
Ros <Fedorovch>
2023-03-01 13:51:05 +0200

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
   # before_save { self.email = email.downcase }
-  before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, 
             format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
@@ -29,13 +30,42 @@ class User < ApplicationRecord
   end
 
   # Возвращает true, если указанный токен соответствует дайджесту. 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token) 
+  # def authenticated?(remember_token)
+  #   return false if remember_digest.nil?
+  #   BCrypt::Password.new(remember_digest).is_password?(remember_token) 
+  # end
+
+  # Возвращает true, если указанный токен соответствует дайджесту. 
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil? 
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Забывает пользователя 
   def forget
     update_attribute(:remember_digest, nil) 
+  end
+
+  # Активирует учетную запись. 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now) 
+  end
+
+  # Посылает письмо со ссылкой на страницу активации. 
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now 
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token 
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230226200908) do
+ActiveRecord::Schema.define(version: 20230228131949) do
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'boolen' for column 'admin'
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
